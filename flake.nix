545f63f4c48d6d356fa9f1a2f1b5ef7b301d2550
Steele Price <steele@thecowboy.ai>
2025-08-21 09:46:08 -0700

    Simplify cim-subject hash, use single entry

--- flake.nix
+++ flake.nix
@@ -52,8 +52,7 @@
               "cim-domain-0.5.0" = "sha256-cz8N9ahxASorc8gMcg+zblfNtcjibtWsvHzbSKjjhaU=";
               "cim-graph-0.1.0" = "sha256-0uaNwhQkLbeMJa25kNPhOSbnm6akD1LHalLGX4Pr6T0=";
               "cim-ipld-0.5.0" = "sha256-Yc2cczSARqegmei6V5+C8ChE/rg89fjHai3npc+PXwk=";
-              "cim-subject-0.5.0 (git+https://github.com/thecowboyai/cim-subject.git?tag=v0.5.0)" = "sha256-Kwxf6YJ42kA3C2kC0er7EvEVTqSixlxSSWCh9fHI5yg=";
-              "cim-subject-0.5.0 (git+https://github.com/TheCowboyAI/cim-subject)" = "sha256-b3Pxh9YHlSIGNjKmINsrIH5JgmRoxcOeH+USoO562SQ=";
+              "cim-subject-0.5.0" = "sha256-Kwxf6YJ42kA3C2kC0er7EvEVTqSixlxSSWCh9fHI5yg=";
             };
           };
           
@@ -85,8 +84,7 @@
               "cim-domain-0.5.0" = "sha256-cz8N9ahxASorc8gMcg+zblfNtcjibtWsvHzbSKjjhaU=";
               "cim-graph-0.1.0" = "sha256-0uaNwhQkLbeMJa25kNPhOSbnm6akD1LHalLGX4Pr6T0=";
               "cim-ipld-0.5.0" = "sha256-Yc2cczSARqegmei6V5+C8ChE/rg89fjHai3npc+PXwk=";
-              "cim-subject-0.5.0 (git+https://github.com/thecowboyai/cim-subject.git?tag=v0.5.0)" = "sha256-Kwxf6YJ42kA3C2kC0er7EvEVTqSixlxSSWCh9fHI5yg=";
-              "cim-subject-0.5.0 (git+https://github.com/TheCowboyAI/cim-subject)" = "sha256-b3Pxh9YHlSIGNjKmINsrIH5JgmRoxcOeH+USoO562SQ=";
+              "cim-subject-0.5.0" = "sha256-Kwxf6YJ42kA3C2kC0er7EvEVTqSixlxSSWCh9fHI5yg=";
             };
           };
           
@@ -215,8 +213,7 @@ EOF
               "cim-domain-0.5.0" = "sha256-cz8N9ahxASorc8gMcg+zblfNtcjibtWsvHzbSKjjhaU=";
               "cim-graph-0.1.0" = "sha256-0uaNwhQkLbeMJa25kNPhOSbnm6akD1LHalLGX4Pr6T0=";
               "cim-ipld-0.5.0" = "sha256-Yc2cczSARqegmei6V5+C8ChE/rg89fjHai3npc+PXwk=";
-              "cim-subject-0.5.0 (git+https://github.com/thecowboyai/cim-subject.git?tag=v0.5.0)" = "sha256-Kwxf6YJ42kA3C2kC0er7EvEVTqSixlxSSWCh9fHI5yg=";
-              "cim-subject-0.5.0 (git+https://github.com/TheCowboyAI/cim-subject)" = "sha256-b3Pxh9YHlSIGNjKmINsrIH5JgmRoxcOeH+USoO562SQ=";
+              "cim-subject-0.5.0" = "sha256-Kwxf6YJ42kA3C2kC0er7EvEVTqSixlxSSWCh9fHI5yg=";
             };
           };
           
